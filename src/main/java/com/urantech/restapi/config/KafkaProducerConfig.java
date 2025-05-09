d173031c87208c91f7b166b6be39eb264e2a7960
Уранов Роман <roman.uranov@gazprombank.ru>
2025-05-09 14:45:24 +0300

    refactored code

--- src/main/java/com/urantech/restapi/config/KafkaProducerConfig.java
+++ src/main/java/com/urantech/restapi/config/KafkaProducerConfig.java
@@ -1,7 +1,5 @@
 package com.urantech.restapi.config;
 
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
